239ed8a2f4ac8ee003c00b6bbd5da4b8310199a6
DenisDK <unworkdk@gmail.com>
2024-11-12 12:19:43 +0200

    User id page + fix bug with delete users

---
+++ app/[locale]/Users/[id]/page.jsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header/Header";
+import React from "react";
+
+const OtherUserProfilePage = () => {
+  return (
+    <>
+      <Header />
+      <div className="max-w-screen-xl mx-auto mt-12 px-3">
+        OtherUserProfilePage
+      </div>
+    </>
+  );
+};
+
+export default OtherUserProfilePage;
